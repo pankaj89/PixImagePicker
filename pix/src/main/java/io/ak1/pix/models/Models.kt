9cb5e93912c3bcc179ef80bbe730267f5ca806d6
PankajSharma <agnipankh.pankaj@gmail.com>
2021-11-06 13:29:28 +0530

    Solved warning

--- pix/src/main/java/io/ak1/pix/models/Models.kt
+++ pix/src/main/java/io/ak1/pix/models/Models.kt
@@ -12,7 +12,6 @@ import java.util.*
  * Created By Akshay Sharma on 17,June,2021
  * https://ak1.io
  */
-@SuppressLint("ParcelCreator")
 @Parcelize
 data class Img(
     var headerDate: String = "",
@@ -22,37 +21,36 @@ data class Img(
 ) : Parcelable {
     @IgnoredOnParcel
     var selected = false
-
     @IgnoredOnParcel
     var position = 0
 }
 
-@SuppressLint("ParcelCreator")
 @Parcelize
-class Options : Parcelable {
-    var ratio = Ratio.RATIO_AUTO
-    var count = 1
-    var spanCount = 4
-    var path = "Pix/Camera"
-    var isFrontFacing = false
-    var mode = Mode.All
-    var flash = Flash.Auto
-    var preSelectedUrls = ArrayList<Uri>()
-    var videoOptions : VideoOptions = VideoOptions()
-}
+data class Options(
+    var ratio: Ratio = Ratio.RATIO_AUTO,
+    var count: Int = 1,
+    var spanCount: Int = 4,
+    var path: String = "Pix/Camera",
+    var isFrontFacing: Boolean = false,
+    var mode: Mode = Mode.All,
+    var flash: Flash = Flash.Auto,
+    var preSelectedUrls: ArrayList<Uri> = ArrayList<Uri>(),
+    var videoOptions: VideoOptions = VideoOptions()
+) : Parcelable
 
 @Parcelize
 enum class Mode : Parcelable {
     All, Picture, Video
 }
+
 @SuppressLint("ParcelCreator")
 @Parcelize
-class VideoOptions : Parcelable {
-    var videoBitrate : Int? = null
-    var audioBitrate : Int? = null
-    var videoFrameRate : Int? = null
-    var videoDurationLimitInSeconds = 10
-}
+data class VideoOptions(
+    var videoBitrate: Int? = null,
+    var audioBitrate: Int? = null,
+    var videoFrameRate: Int? = null,
+    var videoDurationLimitInSeconds: Int = 10
+) : Parcelable
 
 @Parcelize
 enum class Flash : Parcelable {
